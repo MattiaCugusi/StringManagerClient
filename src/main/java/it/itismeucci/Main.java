525ceb8f41687726d522322b6c2249d6fdc59177
lai2-24 <lai2-24@itismeucci.com>
2024-10-14 11:01:25 +0200

    creazione progetto

---
+++ src/main/java/it/itismeucci/Main.java
@@ -0,0 +1,48 @@
+package it.itismeucci;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws UnknownHostException, IOException {
+         System.out.println("Il client e' partito");
+        Socket mySocket = new Socket("localhost", 5672);
+        System.out.println("il client si e' collegato");
+        
+        BufferedReader in = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
+        DataOutputStream out = new DataOutputStream(mySocket.getOutputStream());
+        
+        Scanner sc = new Scanner(System.in);
+        String stringaRicevuta;
+
+        int scelta;
+
+        do{
+            System.out.println("1) Trasformare stringa in maiuscolo");
+            System.out.println("2) Trasformare stringa in minuscolo");
+            System.out.println("3) Ribaltare i caratteri della stringa");
+            System.out.println("4) Contare il numero di caratteri");
+            System.out.println("5) Esci");
+            System.out.println("---------------------------------");
+            System.out.println("inserire il numero dell'operazione che si vuol compiere");
+
+            scelta = sc.nextInt();
+            out.writeBytes(scelta + "\n");
+
+            
+            System.out.println("inserire riga:");
+            String input = sc.nextLine();
+            out.writeBytes(input + "\n");
+            stringaRicevuta = in.readLine();
+            System.out.println(stringaRicevuta);
+
+        }while(scelta != 5);
+
+        mySocket.close();
+    }
+}
